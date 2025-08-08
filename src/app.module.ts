8ea6d27328f30ff9fe0d2ffdd8cd0e18f7942384
Ricky <rickydiaz1802@gmail.com>
2025-08-08 16:44:36 -0500

    3

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { AuthModule } from './auth/auth.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'Bobafeet180',
+      password: 'contrasena',
       database: 'apiinventariodb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize : true
